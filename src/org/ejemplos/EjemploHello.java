82d4374570d322c4bfda09c9c8349664ecb0889f
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2023-05-08 06:30:50 +0200

    Nuevo mensaje añadido

--- src/org/ejemplos/EjemploHello.java
+++ src/org/ejemplos/EjemploHello.java
@@ -5,6 +5,8 @@ public class EjemploHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		System.out.println("Adios");
 	}
 
 }
